58e08e6bfd0a9f663bcf83e4510da953a565cec8
Marcos André <krzgvfs@gmail.com>
2023-09-18 22:17:23 -0300

    Calculo de equações de 2° Grau Implementado.

--- client.py
+++ client.py
@@ -5,16 +5,28 @@ req = rpyc.connect("localhost", 18812)
 try:
     op = int(input("1 - Calcular IMC\n2 - Equação de Segundo Grau\n3 - Palíndromo\nOpção: "))
 
+    # IMC
     if op == 1:
         idade = int(input("\nDigite a idade: "))
         altura = float(input("Digite a altura em metros: "))
-        peso = int(input("Digite o peso: "))
+        peso = float(input("Digite o peso: "))
         
         result = req.root.imc(idade,altura,peso)
         print(result)
 
+    # Equação 2° Grau
     elif op == 2:
-        pass
+        print(" --- Calculo de Equação de 2° Grau ---")
+
+        a = int(input("Coeficiente de x^2 (a): "))
+        if (a != 0):
+            b = int(input("Coeficiente de x (b): "))
+            c = int(input("Termo constante (c): "))
+            raizPositiva, raizNegativa = req.root.equacao2grau(a, b, c)
+            print(f"\nEquação: {a}x²{b}x{c}=0\n\nx1 = {raizPositiva}\nx2 = {raizNegativa}\n\nResultado = ({raizPositiva}; {raizNegativa})\n")
+        else:
+            print("Os fatores informados são caracteristicos de uma equação linear! Por favor, Informe uma Equação de 2° Grau.")
+
     elif op == 3:
         pass
     else: 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import math
 import rpyc
+from fraction import Fraction
 from rpyc.utils.server import ThreadedServer 
 
 class MyApps(rpyc.Service):
@@ -19,9 +20,50 @@ class MyApps(rpyc.Service):
         elif( imc > 40):
             result = "Obesidade grau III"
         return result
+    
+    def exposed_equacao2grau(self, a, b, c):
+        # Trabalhando exclusivamente com Bhaskara
         
-    def exposed_equacao2grau(self, text):
-        pass
+        # Equação de 2° Grau
+        if ( a != 0 and b != 0 and c != 0):
+            print("Solicitado: Equação Completa")
+            # Identificando o delta
+            delta = pow((b),2) - (4 * (a) * (c))
+           
+            if (delta > 0 ): # Possui duas soluções reais distintas para x1​ e x2
+                print("Delta > 0")
+                # Identificando as raizes
+                x1 = Fraction(-b + math.sqrt(delta), (2*a))
+                x2 = Fraction(-b - math.sqrt(delta), (2*a))
+
+                if x1.denominator == 1:
+                    raizPositiva = x1.numerator
+                else:
+                    raizPositiva = x1
+                if x2.denominator == 1:
+                    raizNegativa = x2.numerator
+                else:
+                    raizNegativa = x2
+
+                return raizPositiva, raizNegativa
+
+            elif(delta == 0): # Possui uma solução real (uma raiz dupla) que é igual para x1​ e x2
+                print("Delta == 0")
+                x1 = Fraction(-b, (2 * a))
+
+                if x1.denominator == 1:
+                    raizPositiva = x1.numerator
+                else:
+                    raizPositiva = x1
+
+                return raizPositiva
+
+            elif(delta < 0): # Não possui soluções reais, mas duas soluções complexas conjugadas, uma para x1​ e outra para x2​"
+                print("Delta < 0")
+                ## Essa eu não vou desenrolar não kkkkk
+        
+
+
     def exposed_palindromo(self, text):
         pass
 
