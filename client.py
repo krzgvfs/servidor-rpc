dcfd8ff8a576b0a844bc9d180a0b63a8fd08d9dd
Marcos André <krzgvfs@gmail.com>
2023-09-19 08:22:32 -0300

    Adicionado identificação de Palíndromos

--- client.py
+++ client.py
@@ -3,32 +3,38 @@ import rpyc
 req = rpyc.connect("localhost", 18812)
 
 try:
-    op = int(input("1 - Calcular IMC\n2 - Equação de Segundo Grau\n3 - Palíndromo\nOpção: "))
+    opcao = int(input("1 - Calcular IMC\n2 - Equação de Segundo Grau\n3 - Palíndromo\nOpção: "))
 
     # IMC
-    if op == 1:
+    if opcao == 1:
+        print("--- Calculo de IMC ---")
         idade = int(input("\nDigite a idade: "))
         altura = float(input("Digite a altura em metros: "))
         peso = float(input("Digite o peso: "))
         
-        result = req.root.imc(idade,altura,peso)
-        print(result)
+        resultado = req.root.imc(idade,altura,peso)
+        print(resultado)
 
     # Equação 2° Grau
-    elif op == 2:
+    elif opcao == 2:
+        raizEq = []
         print(" --- Calculo de Equação de 2° Grau ---")
 
         a = int(input("Coeficiente de x^2 (a): "))
         if (a != 0):
             b = int(input("Coeficiente de x (b): "))
             c = int(input("Termo constante (c): "))
-            raizPositiva, raizNegativa = req.root.equacao2grau(a, b, c)
-            print(f"\nEquação: {a}x²{b}x{c}=0\n\nx1 = {raizPositiva}\nx2 = {raizNegativa}\n\nResultado = ({raizPositiva}; {raizNegativa})\n")
+            raizEq = req.root.equacao2grau(a, b, c)
+            print(f"\nEquação: {a}x²{b}x{c}=0\n\nx1 = {raizEq[0]}\nx2 = {raizEq[1]}\n\nResultado = ({raizEq[0]}; {raizEq[1]})\n")
         else:
             print("Os fatores informados são caracteristicos de uma equação linear! Por favor, Informe uma Equação de 2° Grau.")
 
-    elif op == 3:
-        pass
+    elif opcao == 3:
+        print(" --- Palíndromo  ---")
+        palavra = str(input("Digite uma palavra: "))
+        result = req.root.palindromo(palavra)
+        print(f"Resultado: A palavra {palavra} {result}")
+
     else: 
         print("Opção invalida!")
 
--- server.py
+++ server.py
@@ -1,32 +1,33 @@
 import math
+import urllib
 import rpyc
 from fraction import Fraction
 from rpyc.utils.server import ThreadedServer 
 
 class MyApps(rpyc.Service):
     def exposed_imc(self, idade, altura, peso):
+        print("Requisição: IMC")
         imc = peso / (pow(altura, 2))
 
-        if (imc < 18.5):
+        if imc < 18.5:
             result = "Magreza"
-        elif (imc >= 18.5 and imc <= 24.9):
+        elif 18.5 <= imc < 25:
             result = "Normal"
-        elif (imc >= 25 and imc <= 29.9):
+        elif 25 <= imc < 30:
             result = "Sobrepeso"
-        elif (imc >= 30 and imc <= 34.9):
+        elif 30 <= imc < 35:
             result = "Obesidade grau I"
-        elif (imc >= 35 and imc <= 39.9):
+        elif 35 <= imc < 40:
             result = "Obesidade grau II"
-        elif( imc > 40):
+        else:
             result = "Obesidade grau III"
         return result
-    
+
     def exposed_equacao2grau(self, a, b, c):
+        print("Requisição: Equação de 2° Grau")
+        raizEq = []
         # Trabalhando exclusivamente com Bhaskara
-        
-        # Equação de 2° Grau
-        if ( a != 0 and b != 0 and c != 0):
-            print("Solicitado: Equação Completa")
+        if ( a != 0 and b != 0 and c != 0): # Equação Completa.
             # Identificando o delta
             delta = pow((b),2) - (4 * (a) * (c))
            
@@ -37,35 +38,38 @@ class MyApps(rpyc.Service):
                 x2 = Fraction(-b - math.sqrt(delta), (2*a))
 
                 if x1.denominator == 1:
-                    raizPositiva = x1.numerator
+                    raizEq.append(x1.numerator)
                 else:
-                    raizPositiva = x1
+                    raizEq.append(x1)
                 if x2.denominator == 1:
-                    raizNegativa = x2.numerator
+                    raizEq.append(x2.numerator)
                 else:
-                    raizNegativa = x2
+                    raizEq.append(x2)
 
-                return raizPositiva, raizNegativa
+                return raizEq
 
             elif(delta == 0): # Possui uma solução real (uma raiz dupla) que é igual para x1​ e x2
                 print("Delta == 0")
                 x1 = Fraction(-b, (2 * a))
 
                 if x1.denominator == 1:
-                    raizPositiva = x1.numerator
+                    raizEq.apped(x1.numerator)
                 else:
-                    raizPositiva = x1
+                    raizEq.apped(x1)
 
-                return raizPositiva
+                return raizEq
 
             elif(delta < 0): # Não possui soluções reais, mas duas soluções complexas conjugadas, uma para x1​ e outra para x2​"
                 print("Delta < 0")
-                ## Essa eu não vou desenrolar não kkkkk
-        
+                pass
 
-
-    def exposed_palindromo(self, text):
-        pass
+    def exposed_palindromo(self, palavra):
+        print("Requisição: Políndromo")
+        if palavra == palavra[::-1]:
+            result = "é um Palíndromo!\n"
+        else: 
+            result = "não é um Palíndromo!\n"
+        return result
 
 if __name__ == "__main__":
     server = ThreadedServer(MyApps, port = 18812)
